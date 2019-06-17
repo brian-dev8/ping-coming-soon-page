f0275b230a20658dbf84f7cb6aa1c672e9687963
Brian <29547956+brian-dev8@users.noreply.github.com>
2019-06-16 20:04:59 -0400

    TMP: angular form impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ping-coming-soon-page';
+  email = 'Your email address...';
+  emailForm: FormGroup;
+
+  ngOnInit(): void {
+    this.emailForm = new FormGroup({
+      'emailAddr': new FormControl(this.email, [
+        Validators.required,
+        Validators.email
+      ])
+    });
+  }
+
+  get emailAddr() { return this.emailForm.get('emailAddr'); }
+
+  onSubmit() {
+    console.log("Submitted: " + this.emailForm.value);
+  }
 }
